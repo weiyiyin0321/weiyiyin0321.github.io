e18dbd11aa7b5e16c34d6ce257d02cb5548ea63b
beanhead0321 <weiyiyin@gmail.com>
2020-08-06 00:54:47 -0400

    convert charts to horizontal bars

--- scripts/charts.js
+++ scripts/charts.js
@@ -29,9 +29,15 @@ let skill_mapper = function(label, index, labels) {
 }
 
 let skills_data = [
-    [1, 2, 3, 4],
-    [2, 3, 1, 4],
-    [3, 1, 4, 2],
+    [3, 4, 4, 4, 2, 3, 4, 4, 3, 3, 4, 4, 1, 1],
+    [2, 3, 1, 0, 0, 0, 0, 3, 3, 2, 4, 4, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 4, 4, 0, 0],
+]
+
+let abilities_data = [
+    [4, 4, 3, 3, 4, 2, 4, 3, 4, 3, 3, 4, 3, 2],
+    [3, 3, 2, 1, 2, 0, 3, 3, 4, 2, 3, 2, 1, 0],
+    [1, 1, 0, 0, 0, 0, 2, 1, 3, 1, 3, 0, 0, 0]
 ]
 
 let options = {
@@ -50,20 +56,21 @@ let options = {
         xAxes: [{
             gridLines: {
                 offsetGridLines: true, 
+                display: false,
+                drawBorder: false,
+            },
+            ticks: {
+                min: 0,
+                max: 4.5,
+                stepSize: 1,
                 display: false
             }
         }],
         yAxes: [{
             gridLines: {
                 display: false,
-                drawBorder: false,
             },
-            ticks: {
-                min: 0,
-                max: 4.3,
-                stepSize: 1,
-                display: false
-            }
+
         }]
     },
     maintainAspectRatio: false,
@@ -82,7 +89,47 @@ window.onload = function() {
     console.log(window.graph_data_ref, skills_data[window.graph_data_ref]);
 
     let skills_dataset = {
-        labels: ['skill1', 'skill2', 'skill3', 'skill4'],
+        labels: [
+            'Python', 
+            'Pandas', 
+            'Sklearn', 
+            'PySpark', 
+            'Tensorflow', 
+            'Docker', 
+            'Hive', 
+            'SQL', 
+            'R', 
+            'Tableau',
+            'Excel',
+            'PowerPoint',
+            'HTML / CSS',
+            'JavaScript',
+        ],
+        datasets: [{
+            barPercentage: 0.7,
+            barThickness: 'flex',
+            minBarLength: 2,
+            data: skills_data[window.graph_data_ref],
+        }]
+    };
+
+    let abilities_dataset = {
+        labels: [
+            'Regression', 
+            'Classification', 
+            'Clustering', 
+            'Boosting', 
+            'Trees',
+            'Neural Networks', 
+            'Data Analysis', 
+            'Hypothesis Testing', 
+            'Financial Analysis', 
+            'Strategic Marketing', 
+            'Root Cause Analysis',
+            'Customer Targeting',
+            'Automation',
+            'Project Management',
+        ],
         datasets: [{
             barPercentage: 0.7,
             barThickness: 'flex',
@@ -92,10 +139,17 @@ window.onload = function() {
     };
 
 
-    let ctx = document.getElementById('skill-chart').getContext('2d');
-    window.myBar = new Chart(ctx, {
-        type: 'bar',
+    let skill_chart = document.getElementById('skill-chart').getContext('2d');
+    window.skill_bar = new Chart(skill_chart, {
+        type: 'horizontalBar',
         data: skills_dataset,
+        options: options
+    });
+
+    let abilities_chart = document.getElementById('abilities-chart').getContext('2d');
+    window.abilities_bar = new Chart(abilities_chart, {
+        type: 'horizontalBar',
+        data: abilities_dataset,
         options: options
     });
 
@@ -118,10 +172,14 @@ window.addEventListener('scroll', function(e) {
         //pass;
     } else {
         window.graph_data_ref = new_graph_data_ref;
-        window.myBar.data.datasets.forEach(function(dataset) {
+        window.skill_bar.data.datasets.forEach(function(dataset) {
             dataset.data = skills_data[window.graph_data_ref]
         });
-        window.myBar.update();
+        window.abilities_bar.data.datasets.forEach(function(dataset) {
+            dataset.data = abilities_data[window.graph_data_ref]
+        });
+        window.skill_bar.update();
+        window.abilities_bar.update();
     }
 
     
